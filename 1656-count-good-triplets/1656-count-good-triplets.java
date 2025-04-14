1a41c80742dac34e94fd5841e52108384ca1592d
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-14 12:26:59 +0530

    Time: 4 ms (100%), Space: 41.9 MB (6.78%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.java
+++ 1656-count-good-triplets/1656-count-good-triplets.java
@@ -1,27 +1,22 @@
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
         int count = 0;
-        ArrayList<ArrayList<Integer>> values = new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(Math.abs(arr[i] - arr[j]) <= a){
-                    ArrayList<Integer> temp = new ArrayList<>();
-                    temp.add(i);
-                    temp.add(j);
-                    values.add(new ArrayList<>(temp));
+        int[] freq = new int[1001];
+        int n = arr.length;
+        for(int j=0; j<n-1; j++){
+            for(int k=j+1; k<n; k++){
+                if(Math.abs(arr[j] - arr[k]) <= b){
+                    int r = Math.min(arr[j] + a, arr[k] + c);
+                    r = Math.min(r, 1000);
+                    int l = Math.max(arr[j] - a, arr[k] - c);
+                    l = Math.max(l, 0);
+                    if(l <= r){
+                        count += freq[r] - (l == 0 ? 0 : freq[l - 1]);
+                    }
                 }
             }
-        }
-        
-        for(int t=0; t<values.size(); t++){
-            int i = values.get(t).get(0);
-            int j = values.get(t).get(1);
-            int k = j+1;
-
-            for(; k<arr.length; k++){
-                if(Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c){
-                    count++;
-                }
+            for(int i=arr[j]; i<1001; i++){
+                freq[i]++;
             }
         }
         return count;
