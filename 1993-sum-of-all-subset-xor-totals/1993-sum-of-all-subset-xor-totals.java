2506a1809b468b75725d049d2036c825870dd579
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-14 15:14:33 +0530

    Time: 0 ms (100%), Space: 41 MB (56.16%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,13 +1,10 @@
 class Solution {
 
     public int subsets(int[] nums, int n, int start, int xor){
-        int result = xor;
-        for(int i=start; i<n; i++){
-            xor ^= nums[i];
-            result += subsets(nums, n, i+1, xor);
-            xor ^= nums[i];
+        if(start == n){
+            return xor;
         }
-        return result;
+        return subsets(nums, n, start+1, xor^nums[start]) + subsets(nums, n, start+1, xor);
     }
 
     public int subsetXORSum(int[] nums) {
