71fadc133100fa06461b92f0d157c639a0803540
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-23 09:34:57 +0530

    Time: 11 ms (38.71%), Space: 41.2 MB (40.14%) - LeetHub

---
+++ 1500-count-largest-group/1500-count-largest-group.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int countLargestGroup(int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+
+        for(int i=1; i<=n; i++){
+            int sum = 0;
+            int num = i;
+            while(num > 0){
+                sum += num%10;
+                num = num / 10;
+            }
+            if(map.containsKey(sum)){
+                map.put(sum, map.get(sum) + 1);
+                max = Math.max(map.get(sum),max);
+            }else{
+                map.put(sum, 1);
+                max = Math.max(max, 1);
+            }
+        }
+        System.out.println(max);
+        int count = 0;
+        Collection<Integer> values = map.values();
+        for(int i : values){
+            if(i == max){
+                count++;
+            }
+        }
+        return count;
+    }
+}
