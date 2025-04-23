96963373f96d6bf3bcda0459b1a9aa1493fd0ecc
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-23 10:07:14 +0530

    Time: 4 ms (77.06%), Space: 40.8 MB (64.52%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.java
+++ 1500-count-largest-group/1500-count-largest-group.java
@@ -8,22 +8,21 @@ class Solution {
         }
         return sum;
     }
-    
-    public int countLargestGroup(int n) {
-        int[] count = new int[37]; // Max digit sum for n <= 10^4 is 36
-        int max = 0;
 
+    public int countLargestGroup(int n) {
+        int[] count = new int[37];
+        int max = 0;
         for (int i = 1; i <= n; i++) {
             int sum = digitSum(i);
             count[sum]++;
+        }
+        for(int sum=1; sum<37; sum++){
             max = Math.max(max, count[sum]);
         }
-
         int result = 0;
         for (int c : count) {
             if (c == max) result++;
         }
-
         return result;
     }
 }
