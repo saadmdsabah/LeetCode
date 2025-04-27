fc39c73885fefbe0f33d3c270eaf06512af8fa15
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-27 10:57:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            String[] inputArray = input.split("\\s+");
+            int a[] = new int[inputArray.length];
+
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
+
+            Solution ob = new Solution();
+            ob.sort012(a);
+
+            for (int num : a) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+        // code here
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 0){
+                zeros++;
+            }else if(arr[i] == 1){
+                ones++;
+            }else{
+                twos++;
+            }
+        }
+        int i = 0;
+        while(zeros > 0 && i < arr.length){
+            arr[i] = 0;
+            zeros--;
+            i++;
+        }
+        while(ones > 0 && i < arr.length){
+            arr[i] = 1;
+            ones--;
+            i++;
+        }
+        while(twos > 0 && i < arr.length){
+            arr[i] = 2;
+            twos--;
+            i++;
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
