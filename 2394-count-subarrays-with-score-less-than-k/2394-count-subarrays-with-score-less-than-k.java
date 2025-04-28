71e1b0c7b282b4d7aaa6522da86fc99cc1e055f0
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-28 11:31:07 +0530

    Time: 11 ms (5.22%), Space: 56.9 MB (99.69%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.java
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.java
@@ -1,19 +1,74 @@
 class Solution {
-    public long countSubarrays(int[] nums, long k) {
-        int s = 0;
-        int e = 0;
-        long count = 0;
-        long sum = 0;
-
-        while(e < nums.length){
-            sum += nums[e];
-            while(sum*(e - s + 1) >= k){
-                sum -= nums[s];
-                s++;
-            }
-            count += e - s + 1;
-            e++;
+    class Window {
+        int start;
+        int end;
+        Window(int s, int e) {
+            start = s;
+            end = e;
         }
-        return count;
+    }
+    public long countSubarrays(int[] numss, long k) {
+        if (numss.length == 0) {
+            return 0;
+        }
+        long[] nums = new long[numss.length];
+        for (int i = 0 ; i < nums.length; i++) {
+            nums[i] = (long)(numss[i]);
+        }
+
+        int s = 0;
+        while (s < nums.length && nums[s] >= k) {
+            s++;
+        }
+        if (s >= nums.length) {
+            return 0;
+        }
+        int e = s + 1;
+        long currSum = nums[s];
+        List<Window> windows = new ArrayList<>();
+        while (true) {
+            if (e >= nums.length) {
+                if (currSum < k && s < nums.length) {
+                    windows.add(new Window(s, e));
+                }
+                break;
+            }
+            currSum = (currSum / (long)(e - s) + nums[e]) * (long)(e - s + 1);
+            e++;
+            if (currSum >= k) {
+                windows.add(new Window(s, e - 1));
+                while (currSum >= k) {
+                    currSum = (currSum / (long)(e - s) - nums[s]) * (long)(e - s - 1);
+                    s++;
+                    if (s == e) {
+                        while (s < nums.length && nums[s] >= k) {
+                            s++;
+                        }
+                        e = s + 1;
+                        if (s < nums.length) {
+                            currSum = nums[s];
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        long ret = 0l;
+        for (int i = 0; i < windows.size(); i++) {
+            Window w = windows.get(i);
+            //System.out.printf("%d %d\n", w.start, w.end);
+            long len = (long)(w.end - w.start);
+            ret += (1l + len) * len / 2l;
+            if (i > 0) {
+                Window prevW = windows.get(i - 1);
+                if (prevW.end > w.start) {
+                    long foobar = prevW.end - w.start;
+                    ret -= (1l + foobar) * foobar / 2l;
+                }
+            }
+        }
+
+        return ret;
     }
 }
