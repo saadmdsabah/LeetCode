d8605033dc833e72d09d9b4eb1c86cbdfc6d9881
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-28 09:32:10 +0530

    Time: 1 ms (99.49%), Space: 44.9 MB (94.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,24 +1,19 @@
 class Solution {
 
-    public int minJump(int[] nums, int i, int[] dp){
-        if(i >= nums.length-1){
-            return 0;
+    public int jump(int[] arr) {
+        int l = 0;
+        int r = 0;
+        int steps = 0;
+        
+        while(r < arr.length - 1){
+            int farthest = 0;
+            for(int i=l; i<=r; i++){
+                farthest = Math.max(farthest, arr[i] + i);
+            }
+            steps++;
+            l = r + 1;
+            r = farthest;
         }
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        int result = 10000000;
-        for(int j=1; j<=nums[i]; j++){
-            result = Math.min(result, 1 + minJump(nums, i+j, dp));
-        }
-        return dp[i] = result;
-    }
-
-    public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            dp[i] = -1;
-        }
-        return minJump(nums, 0, dp);
+        return steps;
     }
 }
