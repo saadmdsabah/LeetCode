92092f70eda5f604906bf65a9fa38cbe026b2b0a
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-30 16:21:47 +0530

    Time: 14 ms (72.9%), Space: 43.1 MB (76.45%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,16 +1,29 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int result = Integer.MAX_VALUE;
-        for(int i=0; i<nums.length-2; i++){
-            for(int j=i+1; j<nums.length-1; j++){
-                for(int k = j+1; k<nums.length; k++){
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if (Math.abs(sum - target) < Math.abs(result - target)) {
-                        result = sum;
-                    }
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2]; // start with any valid triplet
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (Math.abs(sum - target) < Math.abs(result - target)) {
+                    result = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return target; // exact match found
                 }
             }
         }
+
         return result;
     }
 }
