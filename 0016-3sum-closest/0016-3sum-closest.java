42a3d5ad600bae28f20841a2fa279d9762cbfa1a
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-30 16:22:23 +0530

    Time: 14 ms (72.9%), Space: 43 MB (88.22%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,7 +1,7 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int result = nums[0] + nums[1] + nums[2]; // start with any valid triplet
+        int result = Integer.MAX_VALUE; 
 
         for (int i = 0; i < nums.length - 2; i++) {
             int left = i + 1;
