fb89c8f62176329ba205e7105570256c247b9bf4
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-04-30 13:05:25 +0530

    Time: 17 ms (45.87%), Space: 55.8 MB (36.84%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,32 +1,22 @@
 class Solution {
-
-    public int maxP(int[] prices, int buy, int[][] dp, int index, int fee){
-        if(index == prices.length){
-            return 0;
-        }
-        if(dp[index][buy] != -1){
-            return dp[index][buy];
-        }
-        int profit = 0;
-
-        if(buy == 1){
-            int take = -prices[index] + maxP(prices, 0, dp, index+1, fee);
-            int notTake = maxP(prices, 1, dp, index+1, fee);
-            profit = Math.max(take, notTake);
-        }else{
-            int sell = prices[index] + maxP(prices, 1, dp, index+1, fee) - fee;
-            int notSell = maxP(prices, 0, dp, index+1, fee);
-            profit = Math.max(sell, notSell);
-        }
-
-        return dp[index][buy] = profit;
-    }
-
     public int maxProfit(int[] prices, int fee) {
-        int[][] dp = new int[prices.length][2];
-        for(int i=0; i<prices.length; i++){
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[prices.length+1][2];
+        for(int index=prices.length - 1; index>=0; index--){
+            for(int buy=0; buy<2; buy++){
+                int profit = 0;
+                if(buy == 1){
+                    int take = -prices[index] + dp[index+1][0];
+                    int notTake = dp[index+1][1];
+                    profit = Math.max(take, notTake);
+                }else{
+                    int sell = prices[index] + dp[index+1][1] - fee;
+                    int notSell = dp[index+1][0];
+                    profit = Math.max(sell, notSell);
+                }
+
+                dp[index][buy] = profit;
+            }
         }
-        return maxP(prices, 1, dp, 0, fee);
+        return dp[0][1];
     }
 }
