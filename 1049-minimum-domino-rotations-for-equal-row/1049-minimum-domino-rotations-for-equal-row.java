c2d399fb1d0d06f3ebf39c0392841efaf8f8e2f6
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-03 11:46:19 +0530

    Time: 6 ms (36%), Space: 51.1 MB (6.4%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
@@ -1,31 +1,23 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        if(tops.length != bottoms.length){
-            return -1;
-        }
         int[] map = new int[7];
         for(int i=0; i<tops.length; i++){
             map[tops[i]]++;
             map[bottoms[i]]++;
         }
         int result = Integer.MAX_VALUE;
-
         for(int i=1; i<7; i++){
             if(map[i] >= tops.length){
-                boolean valid = true;
                 int countTops = 0;
                 int countBottoms = 0;
                 for(int j=0; j<tops.length; j++){
                     if(!(tops[j] == i || bottoms[j] == i)){
-                        valid = false;
+                        countTops = Integer.MAX_VALUE;
+                        countBottoms = Integer.MAX_VALUE;
                         break;
                     }else if(tops[j] != i){
                         countTops++;
-                    }
-                }
-                if(!valid) break;
-                for(int j=0; j<bottoms.length; j++){
-                    if(bottoms[j] != i){
+                    }else if(bottoms[j] != i){
                         countBottoms++;
                     }
                 }
