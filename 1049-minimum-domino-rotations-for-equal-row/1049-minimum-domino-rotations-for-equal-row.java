7df6b776dfd2fe3bd86d6df4b17f1d3fb229c1db
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-03 11:58:54 +0530

    Time: 5 ms (54.93%), Space: 50.2 MB (84.8%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
@@ -1,28 +1,24 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int[] map = new int[7];
-        for(int i=0; i<tops.length; i++){
-            map[tops[i]]++;
-            map[bottoms[i]]++;
-        }
+        ArrayList<Integer> possibleCandidates = new ArrayList<>();
+        possibleCandidates.add(tops[0]);
+        possibleCandidates.add(bottoms[0]);
         int result = Integer.MAX_VALUE;
-        for(int i=1; i<7; i++){
-            if(map[i] >= tops.length){
-                boolean valid = true;
-                int countTops = 0;
-                int countBottoms = 0;
-                for(int j=0; j<tops.length; j++){
-                    if(!(tops[j] == i || bottoms[j] == i)){
-                        valid = false;
-                        break;
-                    }else if(tops[j] != i){
-                        countTops++;
-                    }else if(bottoms[j] != i){
-                        countBottoms++;
-                    }
+        for(int i : possibleCandidates){
+            boolean valid = true;
+            int countTops = 0;
+            int countBottoms = 0;
+            for(int j=0; j<tops.length; j++){
+                if(!(tops[j] == i || bottoms[j] == i)){
+                    valid = false;
+                    break;
+                }else if(tops[j] != i){
+                    countTops++;
+                }else if(bottoms[j] != i){
+                    countBottoms++;
                 }
-                if(valid) result = Math.min(result, Math.min(countTops, countBottoms));
             }
+            if(valid) result = Math.min(result, Math.min(countTops, countBottoms));
         }
         return result == Integer.MAX_VALUE ? -1 : result;
     }
