aafc4a22326ad30612c98438e8f6dceb9ab59b63
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-05 19:25:13 +0530

    Time: 2 ms (37.15%), Space: 45.2 MB (25.66%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,40 +1,17 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        if(intervals.length == 0){
-            list.add(new ArrayList<Integer>(Arrays.asList(newInterval[0], newInterval[1])));
-            int[][] result = new int[list.size()][2];
-            for(int i=0; i<result.length; i++){
-                result[i][0] = list.get(i).get(0);
-                result[i][1] = list.get(i).get(1);
-            }
-            return result;
-        }
         int previousEnd = Integer.MIN_VALUE;
         boolean taken = false;
+        int i = 0;
 
-        for(int i=0; i<intervals.length; i++){
+        for(; i<intervals.length; i++){
             if(newInterval[0] > previousEnd && newInterval[1] < intervals[i][0]){
                 list.add(new ArrayList<Integer>(Arrays.asList(newInterval[0], newInterval[1])));
-                for(; i<intervals.length; i++){
-                    list.add(new ArrayList<Integer>(Arrays.asList(intervals[i][0], intervals[i][1])));
-                }
                 taken = true;
                 break;
-            }else if(intervals[i][0] <= newInterval[0] && newInterval[0] <= intervals[i][1]){
-                int newStart = intervals[i][0];
-                int newEnd = Math.max(intervals[i][1], newInterval[1]);
-                while(i < intervals.length && intervals[i][0] <= newEnd){
-                    newEnd = Math.max(newEnd, intervals[i][1]);
-                    i++;
-                }
-                taken = true;
-                list.add(new ArrayList<Integer>(Arrays.asList(newStart, newEnd)));
-                for(; i<intervals.length; i++){
-                    list.add(new ArrayList<Integer>(Arrays.asList(intervals[i][0], intervals[i][1])));
-                }
-                break;
-            }else if(newInterval[0] <= intervals[i][0] && intervals[i][0] <= newInterval[1]){
+            }else if((intervals[i][0] <= newInterval[0] && newInterval[0] <= intervals[i][1]) || 
+            (newInterval[0] <= intervals[i][0] && intervals[i][0] <= newInterval[1])){
                 int newStart = Math.min(intervals[i][0], newInterval[0]);
                 int newEnd = Math.max(intervals[i][1], newInterval[1]);
                 while(i < intervals.length && intervals[i][0] <= newEnd){
@@ -43,9 +20,6 @@ class Solution {
                 }
                 taken = true;
                 list.add(new ArrayList<Integer>(Arrays.asList(newStart, newEnd)));
-                for(; i<intervals.length; i++){
-                    list.add(new ArrayList<Integer>(Arrays.asList(intervals[i][0], intervals[i][1])));
-                }
                 break;
             }else{
                 list.add(new ArrayList<Integer>(Arrays.asList(intervals[i][0], intervals[i][1])));
@@ -53,14 +27,17 @@ class Solution {
             }
         }
 
+        for(; i<intervals.length; i++){
+            list.add(new ArrayList<Integer>(Arrays.asList(intervals[i][0], intervals[i][1])));
+        }
+        
         if(!taken){
             list.add(new ArrayList<Integer>(Arrays.asList(newInterval[0], newInterval[1])));
         }
-
         int[][] result = new int[list.size()][2];
-        for(int i=0; i<result.length; i++){
-            result[i][0] = list.get(i).get(0);
-            result[i][1] = list.get(i).get(1);
+        for(int j=0; j<result.length; j++){
+            result[j][0] = list.get(j).get(0);
+            result[j][1] = list.get(j).get(1);
         }
         return result;
     }
