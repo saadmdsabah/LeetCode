d86b5c48ce637867f464320bf53b33e90be37026
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-06 12:35:29 +0530

    Time: 0 ms (100%), Space: 41.9 MB (15.7%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int min = 0;
+        int max = 0;
+        for(int i=0; i<s.length(); i++){
+            char curr = s.charAt(i);
+            if(curr == '('){
+                min++;
+                max++;
+            }else if(curr == ')'){
+                min--;
+                max--;
+            }else{
+                min--;
+                max++;
+            }
+            if(min < 0){
+                min = 0;
+            }
+            if(max < 0){
+                return false;
+            }
+        }
+        return min==0;
+    }
+}
