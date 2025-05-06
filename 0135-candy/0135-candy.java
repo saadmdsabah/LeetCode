11aaffbc0b23f405c19cdbeb70e9930871a2aed2
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-06 14:52:20 +0530

    Time: 4 ms (26.45%), Space: 46.1 MB (34.15%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,7 +1,6 @@
 class Solution {
     public int candy(int[] ratings) {
         int[] left = new int[ratings.length];
-        int[] right = new int[ratings.length];
         for(int i=0; i<ratings.length; i++){
             if(i - 1 >= 0){
                 if(ratings[i] > ratings[i-1]){
@@ -14,21 +13,19 @@ class Solution {
             }
         }
 
-        for(int i=ratings.length-1; i>=0; i--){
-            if(i + 1 < ratings.length){
-                if(ratings[i] > ratings[i+1]){
-                    right[i] = right[i+1] + 1;
-                }else{
-                    right[i] = 1;
-                }
+        int sum = Math.max(1, left[ratings.length - 1]);
+        int curr = 1;
+        int prev = 1;
+        for(int i=ratings.length-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                curr = prev + 1;
+                prev = curr;
             }else{
-                right[i] = 1;
+                curr = 1;
+                prev = 1;
             }
+            sum += Math.max(curr, left[i]);
         }
-        int result = 0;
-        for(int i=0; i<ratings.length; i++){
-            result += Math.max(left[i], right[i]);
-        }
-        return result;
+        return sum;
     }
 }
