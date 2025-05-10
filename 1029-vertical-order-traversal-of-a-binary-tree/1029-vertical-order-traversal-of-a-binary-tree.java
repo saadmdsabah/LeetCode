18c6511f948179c84d368cdd3bfdbfe425b4cf7e
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-10 19:28:52 +0530

    Time: 3 ms (82.54%), Space: 42.8 MB (35.41%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    class Tuple {
+        TreeNode node;
+        int row, col;
+
+        public Tuple(TreeNode node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Tuple> q = new LinkedList<>();
+        q.offer(new Tuple(root, 0, 0)); 
+
+        while (!q.isEmpty()) {
+            Tuple tuple = q.poll();
+            TreeNode node = tuple.node;
+            int row = tuple.row;
+            int col = tuple.col;
+
+            map.putIfAbsent(col, new TreeMap<>());
+            map.get(col).putIfAbsent(row, new PriorityQueue<>());
+            map.get(col).get(row).offer(node.val);
+
+            if (node.left != null) {
+                q.offer(new Tuple(node.left, row + 1, col - 1));
+            }
+            if (node.right != null) {
+                q.offer(new Tuple(node.right, row + 1, col + 1));
+            }
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            List<Integer> colList = new ArrayList<>();
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    colList.add(nodes.poll());
+                }
+            }
+            result.add(colList);
+        }
+
+        return result;
+    }
+}
