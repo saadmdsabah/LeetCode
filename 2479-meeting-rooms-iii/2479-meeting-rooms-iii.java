0428c7d608fbfc0514032aa225b1b7271f65fd7c
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-12 17:38:16 +0530

    Time: 68 ms (78.07%), Space: 98.6 MB (55.2%) - LeetHub

--- 2479-meeting-rooms-iii/2479-meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.java
@@ -1,38 +1,56 @@
 class Solution {
+
+    class Pair implements Comparable<Pair> {
+        int room;
+        int endTime;
+        public Pair(int room, int endTime){
+            this.room = room;
+            this.endTime = endTime;
+        }
+
+        public int compareTo(Pair p){
+            if(this.endTime == p.endTime){
+                return this.room - p.room;
+            }
+            return this.endTime - p.endTime;
+        }
+    }
+
+
     public int mostBooked(int n, int[][] meetings) {
         int[] roomCount = new int[n];
-        long[] roomLastEndTime = new long[n];
         Arrays.sort(meetings, (a,b)-> Integer.compare(a[0], b[0]));
+        PriorityQueue<Pair> nextAvailableRoom = new PriorityQueue<>();
+        PriorityQueue<Integer> roomAllotment = new PriorityQueue<>();
+
+        for(int i=0; i<n; i++){
+            roomAllotment.add(i);
+        }
 
         for(int i=0; i<meetings.length; i++){
             int start = meetings[i][0];
             int end = meetings[i][1];
-            long minEndTime = Long.MAX_VALUE;
-            int minRoomNumber = 0;
-            boolean available = false;
 
-            for(int room=0; room<n; room++){
-                if(roomLastEndTime[room] <= start){
-                    roomLastEndTime[room] = end;
-                    roomCount[room]++;
-                    available = true;
-                    break;
-                }
-
-                if(minEndTime > roomLastEndTime[room]){
-                    minEndTime = roomLastEndTime[room];
-                    minRoomNumber = room;
-                }
+            while(!nextAvailableRoom.isEmpty() && nextAvailableRoom.peek().endTime <= start){
+                Pair availableRoom = nextAvailableRoom.remove();
+                int roomNumber = availableRoom.room;
+                roomAllotment.add(roomNumber);
             }
-
-            if(!available){
-                roomLastEndTime[minRoomNumber] = minEndTime + end - start;
-                roomCount[minRoomNumber]++;
+            if(roomAllotment.isEmpty()){
+                Pair nextAvailable = nextAvailableRoom.remove();
+                int availableRoomNumber = nextAvailable.room;
+                int endTimeForCurrentRoom = nextAvailable.endTime;
+                roomCount[availableRoomNumber]++;
+                nextAvailableRoom.add(new Pair(availableRoomNumber, endTimeForCurrentRoom + (end - start)));
+            }else{
+                int availableRoomNumber = roomAllotment.remove();
+                roomCount[availableRoomNumber]++;
+                nextAvailableRoom.add(new Pair(availableRoomNumber, end));
             }
         }
 
-        int roomNumber = 0;
         int max = 0;
+        int roomNumber = 0;
         for(int i=0; i<n; i++){
             if(roomCount[i] > max){
                 max = roomCount[i];
