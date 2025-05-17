d8dfbccec06c3e0061d7acdef2dabc303ba1f610
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-17 14:50:53 +0530

    Time: 1 ms (47.7%), Space: 44.9 MB (83.07%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -3,43 +3,42 @@ class Solution {
     class Pair{
         int x;
         int y;
-        public Pair(int x, int y){
+        int val;
+        public Pair(int x, int y, int val){
             this.x = x;
             this.y = y;
+            this.val = val;
         }
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n = image.length;
         int m = image[0].length;
+
         boolean[][] visited = new boolean[n][m];
-        int[][] result = new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                result[i][j] = image[i][j];
-            }
-        }
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr, sc));
-        result[sr][sc] = color;
+        q.add(new Pair(sr, sc, image[sr][sc]));
         visited[sr][sc] = true;
+        image[sr][sc] = color;
 
         while(!q.isEmpty()){
             Pair currPair = q.remove();
             int x = currPair.x;
             int y = currPair.y;
+            int val  = currPair.val;
+
             int[] xd = {-1,1,0,0};
             int[] yd = {0,0,-1,1};
             for(int i=0; i<4; i++){
                 int newX = x + xd[i];
                 int newY = y + yd[i];
-                if(newX >= 0 && newX < n && newY >=0 && newY < m && image[newX][newY] == image[x][y] && !visited[newX][newY]){
-                    result[newX][newY] = color;
-                    q.add(new Pair(newX, newY));
+                if(newX >= 0 && newX < n && newY >=0 && newY < m && image[newX][newY] == val && !visited[newX][newY]){
+                    image[newX][newY] = color;
+                    q.add(new Pair(newX, newY, val));
                     visited[newX][newY] = true;
                 }
             }
         }
-        return result;
+        return image;
     }
 }
