006dd2f32dead58baba74421d87ce442450ffd9d
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-19 08:31:22 +0530

    Time: 1 ms (34.56%), Space: 42 MB (74.91%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.java
+++ 3321-type-of-triangle/3321-type-of-triangle.java
@@ -1,7 +1,6 @@
 class Solution {
     public String triangleType(int[] nums) {
-        Arrays.sort(nums);
-        if(nums[0] + nums[1] <= nums[2]){
+        if(nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]){
             return "none";
         }else if(nums[0] == nums[1] && nums[1] == nums[2]){
             return "equilateral";
