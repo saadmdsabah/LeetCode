c7c43f95583943b2685ece4c284ae8ff5b1e1acb
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-23 12:06:30 +0530

    Time: 34 ms (5.26%), Space: 52.5 MB (97.37%) - LeetHub

---
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        Long[] newNums = new Long[nums.length]; 
+        long sum = 0;
+
+        for (int i : nums) sum += i;
+
+        for (int i = 0; i < nums.length; i++) {
+            newNums[i] = (long)((nums[i] ^ k) - nums[i]);
+        }
+        Arrays.sort(newNums, Collections.reverseOrder()); 
+
+        for (int i = 0; i < nums.length - 1; i+=2) {
+            long newProfit = newNums[i] + newNums[i + 1];
+            if (newProfit > 0) {
+                sum += newProfit;
+            }
+        }
+        return sum;
+    }
+}
