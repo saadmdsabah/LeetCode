17a980b4529f26682cb37cfded836a0e62483fe9
Saad Sabahuddin <103499208+saadmdsabah@users.noreply.github.com>
2025-05-25 09:58:19 +0530

    Time: 66 ms (51.42%), Space: 58.7 MB (40.24%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,41 +1,38 @@
 class Solution {
     public int longestPalindrome(String[] words) {
         HashMap<String, Integer> map = new HashMap<>();
-
-        for(int i=0; i<words.length; i++){
-            if(map.containsKey(words[i])){
-                map.put(words[i], map.get(words[i]) + 1);
-            }else{
-                map.put(words[i], 1);
-            }
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
         int count = 0;
-        boolean odd = false;
+        boolean hasOddCenter = false;
 
-        for(int i=0; i<words.length; i++){
-            StringBuilder temp = new StringBuilder(words[i]);
-            temp.reverse();
-            String newString = temp.toString();
-            if(map.containsKey(newString)){
-                if(newString.charAt(0) == newString.charAt(1)){
-                    if(map.get(newString) >= 2){
-                        count += 4;
-                        map.put(newString, map.get(newString) - 2);
-                    }else if(map.get(newString) != 0 && !odd){
-                        count += 2;
-                        odd = true;
-                        map.put(newString, map.get(newString) - 1);
-                    }
-                }else{
-                    if(map.containsKey(words[i]) && map.get(words[i]) != 0 && map.get(newString) != 0){
-                        count += 4;
-                        map.put(words[i], map.get(words[i]) - 1);
-                        map.put(newString, map.get(newString) - 1);
-                    }
+        for (String word : map.keySet()) {
+            String reversed = new StringBuilder(word).reverse().toString();
+
+            // Case 1: symmetric word like "gg"
+            if (word.equals(reversed)) {
+                int freq = map.get(word);
+                count += (freq / 2) * 4;
+                if (freq % 2 == 1) {
+                    hasOddCenter = true;
                 }
+            } 
+            // Case 2: word and its reverse (e.g., "ab" & "ba")
+            else if (map.containsKey(reversed)) {
+                int pairs = Math.min(map.get(word), map.get(reversed));
+                count += pairs * 4;
+                // To prevent double-counting
+                map.put(word, 0);
+                map.put(reversed, 0);
             }
         }
+
+        if (hasOddCenter) {
+            count += 2;  // center piece
+        }
+
         return count;
     }
 }
